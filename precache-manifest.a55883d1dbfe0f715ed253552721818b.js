140c6c8b432bc39b2466ede690472e9a3497fce9
Datub2002 <you@example.com>
2022-04-28 00:32:12 +0700

    Updates

---
+++ precache-manifest.a55883d1dbfe0f715ed253552721818b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e9ac5b57570bc751568437505637b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64eae0949b6a3360402c",
+    "url": "/static/css/main.ab08b8d4.chunk.css"
+  },
+  {
+    "revision": "c441a6a3dd5a599cf811",
+    "url": "/static/js/2.df4c158d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df4c158d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64eae0949b6a3360402c",
+    "url": "/static/js/main.bed091fc.chunk.js"
+  },
+  {
+    "revision": "16b33f2b0eb07435af46",
+    "url": "/static/js/runtime-main.89d0faaf.js"
+  },
+  {
+    "revision": "f51445d434c18ccb6868449d2f5ad5d5",
+    "url": "/static/media/black-car.f51445d4.jpg"
+  }
+]);
